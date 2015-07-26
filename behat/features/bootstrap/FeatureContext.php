d15b3a25e4a497c815a00ecc34c1abbe34e44331
Amitaibu <amitai@gizra.com>
2015-07-26 16:43:11 +0300

    Add behat tests

--- behat/features/bootstrap/FeatureContext.php
+++ behat/features/bootstrap/FeatureContext.php
@@ -12,27 +12,98 @@ class FeatureContext extends MinkContext implements SnippetAcceptingContext {
    * @Given I am an anonymous user
    */
   public function iAmAnAnonymousUser() {
-    // Just let this pass-through.
-  }
-
-  /**
-   * @When I visit the homepage
-   */
-  public function iVisitTheHomepage() {
+    // Set cookies.
     $this->getSession()->visit($this->locatePath('/'));
+    $this->getSession()->setcookie('notShippedNotice', 'yes');
+    $this->getSession()->setcookie('is_18', 'yes');
+
+    $this->getSession()->setcookie('country_code', 'US');
   }
 
   /**
-   * @Then I should have access to the page
+   * @When I add to cart
    */
-  public function iShouldHaveAccessToThePage() {
-    $this->assertSession()->statusCodeEquals('200');
+  public function iAddToCart() {
+    // Add to cart.
+    $this->iWaitForCssElement('#DesktopBuyNowButtonProdPage');
+
+    $element = $this->getSession()->getPage()->find('css', '#DesktopBuyNowButtonProdPage');
+    $element->click();
   }
 
   /**
-   * @Then I should not have access to the page
+   * @Then I should see the item added to the cart
    */
-  public function iShouldNotHaveAccessToThePage() {
-    $this->assertSession()->statusCodeEquals('403');
+  public function iShouldSeeTheItemAddedToTheCart() {
+    $this->getSession()->visit($this->locatePath('/catalog/index.php?_g=co&_a=cart'));
+
+    // Find the checkout button.
+    $this->iWaitForCssElement('#sc_ready_button_footer');
+  }
+
+  /**
+   * @Then I should wait for the text :arg1 to :arg2
+   */
+  public function iShouldWaitForTheTextTo($text, $appear) {
+    $this->waitForXpathNode(".//*[contains(normalize-space(string(text())), \"$text\")]", $appear == 'appear');
+  }
+
+  /**
+   * @Then /^I wait for css element "([^"]*)" to "([^"]*)"$/
+   */
+  public function iWaitForCssElement($element, $appear = 'appear') {
+    $xpath = $this->getSession()->getSelectorsHandler()->selectorToXpath('css', $element);
+    $this->waitForXpathNode($xpath, $appear == 'appear');
+  }
+
+
+  /**
+   * Helper function; Execute a function until it return TRUE or timeouts.
+   *
+   * @param $fn
+   *   A callable to invoke.
+   * @param int $timeout
+   *   The timeout period. Defaults to 10 seconds.
+   *
+   * @throws Exception
+   */
+  private function waitFor($fn, $timeout = 15000) {
+    $start = microtime(true);
+    $end = $start + $timeout / 1000.0;
+    while (microtime(true) < $end) {
+      if ($fn($this)) {
+        return;
+      }
+    }
+    throw new \Exception('waitFor timed out.');
+  }
+
+  /**
+   * Wait for an element by its XPath to appear or disappear.
+   *
+   * @param string $xpath
+   *   The XPath string.
+   * @param bool $appear
+   *   Determine if element should appear. Defaults to TRUE.
+   *
+   * @throws Exception
+   */
+  private function waitForXpathNode($xpath, $appear = TRUE) {
+    $this->waitFor(function($context) use ($xpath, $appear) {
+      try {
+        $nodes = $context->getSession()->getDriver()->find($xpath);
+        if (count($nodes) > 0) {
+          $visible = $nodes[0]->isVisible();
+          return $appear ? $visible : !$visible;
+        }
+        return !$appear;
+      }
+      catch (WebDriver\Exception $e) {
+        if ($e->getCode() == WebDriver\Exception::NO_SUCH_ELEMENT) {
+          return !$appear;
+        }
+        throw $e;
+      }
+    });
   }
 }
